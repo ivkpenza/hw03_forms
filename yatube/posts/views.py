72a3c8c87582568c494437298de564e98015ef84
Иван Кузнецов <ivkpenza@gmail.com>
2022-09-27 15:27:19 +0300

    123

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -50,6 +50,7 @@ def post_create(request):
             post.author = request.user
             post.save()
             return redirect('posts:profile', request.user.username)
+        form = PostForm()
     context = {
         'form': form,
     }
